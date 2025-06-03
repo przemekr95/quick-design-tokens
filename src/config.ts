a25713627d195453b0b6f505226ee3f68dbb6539
przemekr95 <przemekrura95@gmail.com>
2025-06-03 19:17:16 +0200

    Added new version

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,19 @@
-import StyleDictionary, { Dictionary } from 'style-dictionary';
+import StyleDictionary from 'style-dictionary';
 
 // TODO: Dodaj konfigurację dla formatów CJS/ESM:
 // - Dodaj format 'javascript/es6' dla ESM
 // - Dodaj format 'javascript/cjs' dla CommonJS
 // - Dodaj format 'typescript/es6-declarations' dla typów TypeScript
 // - Rozważ dodanie custom formatów dla React Native, iOS, Android
+
+interface FileOptions {
+  destination: string;
+  format: string;
+  options?: {
+    showFileHeader?: boolean;
+    fileHeader?: () => string[];
+  };
+}
 
 interface ProjectConfig {
   name: string;
@@ -13,26 +22,12 @@ interface ProjectConfig {
     scss: {
       transformGroup: string;
       buildPath: string;
-      files: Array<{
-        destination: string;
-        format: string;
-        options?: {
-          showFileHeader?: boolean;
-          fileHeader?: (fileHeader: { file: { destination: string }; [key: string]: any }) => string[];
-        };
-      }>;
+      files: FileOptions[];
     };
     css: {
       transformGroup: string;
       buildPath: string;
-      files: Array<{
-        destination: string;
-        format: string;
-        options?: {
-          showFileHeader?: boolean;
-          fileHeader?: (fileHeader: { file: { destination: string }; [key: string]: any }) => string[];
-        };
-      }>;
+      files: FileOptions[];
     };
   };
 }
@@ -122,9 +117,9 @@ export const configs = {
 // Custom format dla klas SCSS z fontami
 StyleDictionary.registerFormat({
   name: 'scss/font-classes',
-  format: function({ dictionary, options, file }: { dictionary: Dictionary; options?: any; file?: any }) {
+  format: function({ dictionary, options }): string {
     const header = options?.showFileHeader && options?.fileHeader ? 
-      options.fileHeader({ file }).map((line: string) => `// ${line}`).join('\n') + '\n\n' : '';
+      options.fileHeader().map((line: string) => `// ${line}`).join('\n') + '\n\n' : '';
     
     let output = header;
     
@@ -160,9 +155,9 @@ StyleDictionary.registerFormat({
 // Custom format dla klas CSS z fontami
 StyleDictionary.registerFormat({
   name: 'css/font-classes',
-  format: function({ dictionary, options, file }: { dictionary: Dictionary; options?: any; file?: any }) {
+  format: function({ dictionary, options }): string {
     const header = options?.showFileHeader && options?.fileHeader ? 
-      options.fileHeader({ file }).map((line: string) => `/* ${line} */`).join('\n') + '\n\n' : '';
+      options.fileHeader().map((line: string) => `/* ${line} */`).join('\n') + '\n\n' : '';
     
     let output = header;
     
@@ -198,9 +193,9 @@ StyleDictionary.registerFormat({
 // Custom formaty - można rozszerzyć w przyszłości  
 StyleDictionary.registerFormat({
   name: 'scss/variables-with-map',
-  format: function({ dictionary, options, file }: { dictionary: Dictionary; options?: any; file?: any }) {
+  format: function({ dictionary, options }): string {
     const header = options?.showFileHeader && options?.fileHeader ? 
-      options.fileHeader({ file }).map((line: string) => `// ${line}`).join('\n') + '\n\n' : '';
+      options.fileHeader().map((line: string) => `// ${line}`).join('\n') + '\n\n' : '';
     
     let output = header;
     
